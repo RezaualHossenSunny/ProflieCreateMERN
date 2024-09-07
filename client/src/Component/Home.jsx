c2fb108dddd2221d5779814e72d2157ace830ab7
RezaualHossenSunny <sunnyrahman542@gmail.com>
2024-09-07 23:25:53 +0600

    useloder cliend

--- client/src/Component/Home.jsx
+++ client/src/Component/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const Home = () => {
+
+  const data = useLoaderData()
   return (
-    <div>Home</div>
+    <div>Home {data.length}</div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,38 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Home from './Component/Home.jsx';
-import CreateUser from './Component/CreateUser.jsx';
-import Upadate from './Component/Upadate.jsx';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Component/Home.jsx";
+import CreateUser from "./Component/CreateUser.jsx";
+import Upadate from "./Component/Upadate.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
- children:[{
-  path:"/",
-  element: <Home/>
- },{
-  path: "/createuser",
-  element: <CreateUser/>
- },{
-  path:"/uapdate",
-  element:<Upadate/>
- }
-
-]
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader:()=> fetch('http://localhost:5000/users')
+      },
+      {
+        path: "/createuser",
+        element: <CreateUser />,
+      },
+      {
+        path: "/uapdate",
+        element: <Upadate />,
+      },
+    ],
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
